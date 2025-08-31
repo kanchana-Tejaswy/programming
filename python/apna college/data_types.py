3d47c1a4fecac49cd6b94309bba58b37812a05b8
Kanchana.Tejaswy <mail2tejaswy@gmail.com>
2025-08-31 13:55:46 +0530

    data type

---
+++ python/apna college/data_types.py
@@ -0,0 +1,8 @@
+name1 = "ss"
+name2 = 'ss'
+name3 = '''ss'''
+
+print(name1)
+print(name2)
+print(name3)
+
---
+++ python/apna college/keywords.py
@@ -0,0 +1,6 @@
+#                                       key words in python 
+
+
+#  else   . in         . return . as      . except . is   . True    .  assert  .  finally . lambda . try   . break 
+#  False  . nonlocal   . with   . class   . for    . None . while   . continue . from     . not    . yield . def .
+#  global . or         . del    . if      . pass   . eilf . import  . raise    . 
