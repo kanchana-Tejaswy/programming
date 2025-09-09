42260b071ae5cfe751b00dce1dbec1e895c32cfe
Kanchana.Tejaswy <mail2tejaswy@gmail.com>
2025-09-09 23:50:28 +0530

    debug code

--- python/leetcode/daynight.py
+++ python/leetcode/daynight.py
@@ -2,18 +2,17 @@ class Solution(object):
     def peopleAwareOfSecret(self, n, delay, forget):
         MOD = 10**9 + 7
         dp = [0] * (n + 1)
-        dp[1] = 1
-
+        dp[1] = 110
         share = 0  
 
         for day in range(2, n + 1):
-       
+           
             if day - delay >= 1:
                 share = (share + dp[day - delay]) % MOD
-        
+           
             if day - forget >= 1:
                 share = (share - dp[day - forget]) % MOD
-            
+           
             dp[day] = share % MOD
 
        
