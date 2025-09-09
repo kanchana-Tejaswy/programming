31edaadb138c570ff13fe9f51f389c399e8a4ed8
Kanchana.Tejaswy <mail2tejaswy@gmail.com>
2025-09-09 23:32:01 +0530

    added folder  for pyhton to save the leet code solved problems

---
+++ java/geeks for geeks/skeletoncode.java
@@ -0,0 +1,5 @@
+class skeletoncode{
+    public static void main(String[]  args){
+        // this is the skeletion code or java 
+    }
+}
---
+++ python/leetcode/daynight.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def peopleAwareOfSecret(self, n, delay, forget):
+        MOD = 10**9 + 7
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        share = 0  
+
+        for day in range(2, n + 1):
+       
+            if day - delay >= 1:
+                share = (share + dp[day - delay]) % MOD
+        
+            if day - forget >= 1:
+                share = (share - dp[day - forget]) % MOD
+            
+            dp[day] = share % MOD
+
+       
+        start = max(1, n - forget + 1)
+        return sum(dp[start:n + 1]) % MOD
